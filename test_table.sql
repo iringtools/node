9111eff4035141178ff2aa3471da5ac7b4c2e786
Rob DeCarlo <rdecarlo73@gmail.com>
2016-07-09 21:40:27 -0400

    cleanup

--- test_table.sql
+++ test_table.sql
@@ -1,5 +1,7 @@
-CREATE TABLE   
-    test   
-    ( major varchar(20), minor varchar(20), revision varchar(20) );
+CREATE TABLE test ()
+    major VARCHAR(20), 
+    minor VARCHAR(20), 
+    revision VARCHAR(20) 
+);
 
-insert into dbo.test values ('1', '2', '3');
+INSERT INTO test VALUES ('1', '2', '3');
